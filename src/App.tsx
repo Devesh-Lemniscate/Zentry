fac63de3c7b13586a5a5c65a90e8f0e2e05a6f81
Devesh Tiwari <tiwaridevesh444@gmail.com>
2025-08-06 17:08:16 +0530

    Hero Section Complete

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <div className="relative min-w-screen w-screen overflow-x-hidden bg-gray-900">
       <Hero />
+      
+      <section className="z-0 min-h-screen bg-blue-500"></section>
     </div>
 )
 }
