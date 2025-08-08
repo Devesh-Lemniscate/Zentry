72c7857007b20448fee7b79c601da734c205b896
Devesh Tiwari <tiwaridevesh444@gmail.com>
2025-08-08 12:26:27 +0530

    Navbar complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <div className="relative min-w-screen w-screen overflow-x-hidden bg-sky-400">
+      <Navbar/>
       <Hero />
       <About/>
     </div>
