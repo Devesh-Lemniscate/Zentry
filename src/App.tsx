cb9ceca4d4060f41ddabf040df07bf22e89bd4c1
Devesh Tiwari <tiwaridevesh444@gmail.com>
2025-08-10 13:15:14 +0530

    Added feature section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
+import Features from "./components/Features"
+import NavBar from "./components/Navbar"
+
 
 const App = () => {
   return (
     <div className="relative min-w-screen w-screen overflow-x-hidden bg-sky-400">
+      <NavBar/>
       <Hero />
       <About/>
+      <Features/>
     </div>
 )
 }
